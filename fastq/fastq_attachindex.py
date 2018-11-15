609ec716ea9d2134af5989bd7c3f54445bafec4e
Samuel Wittwer <samwittw@gmail.com>
2018-11-15 13:12:34 +0100

    Added fastq attachindex script (not yet finished) and fastq testdata

---
+++ fastq/fastq_attachindex.py
@@ -0,0 +1,60 @@
+import getopt
+import sys
+
+
+def loadargs():
+    """This function loads the command line arguments and starts main"""
+    readfile = None
+    indexfile = None
+    outputfile = None
+    try:
+        opts, _ = getopt.getopt(sys.argv[1:], 'r:i:o:h', ['reads', 'index', 'output', 'help'])
+    except getopt.GetoptError:
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ('-r', '--reads'):
+            readfile = arg
+        if opt in ('-i', '--index'):
+            indexfile = arg
+        if opt in ('-o', '--output'):
+            outputfile = arg
+        if opt in ('-h', '--help'):
+            help()
+    main(readfile, indexfile, outputfile)
+
+
+def fastqtodict(fastqfile):
+    """generic function to read fastq. Ignores Q score line. in: fastq filename out: dict {readname:seq}"""
+    with open(fastqfile) as i:
+        counter = None
+        readname = None
+        seqdict = {}
+        for line in i:
+            if line.startswith('@'):
+                readname = line.split(' ')[0]
+                counter = 1
+            if counter == 2:
+                seqdict[readname] = line.strip()
+            counter += 1
+    return seqdict
+
+
+def help():
+    print 'HELP'
+
+
+def main(readfile, indexfile, outputfile):
+    indexdict = fastqtodict(indexfile)
+    seqdict = fastqtodict(readfile)
+
+    print len(indexdict)
+    print len(seqdict)
+
+
+if __name__ == '__main__':
+    loadargs()
+
+
+#TODO: attach index sequence to read name. fastq reading works.
+
+#TODO: test with missing indexes (fewer indexes than reads -> robust to filtering)
