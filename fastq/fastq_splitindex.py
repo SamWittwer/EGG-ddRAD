f8799de0c65d31d59a398b3990543ac989c53bef
Samuel Wittwer <samwittw@gmail.com>
2018-11-28 07:14:26 +0100

    getopt fixed

--- fastq/fastq_splitindex.py
+++ fastq/fastq_splitindex.py
@@ -8,9 +8,11 @@ def loadargs():
     readfile = None
     illumina_idx = None
     outputfile = None
+    print 'STARTING'
     try:
-        opts, _ = getopt.getopt(sys.argv[1:], 'r:i:o:h', ['reads', 'index', 'output', 'help'])
+        opts, _ = getopt.getopt(sys.argv[1:], 'r:i:o:p:h', ['reads', 'index', 'output', 'prefix', 'help'])
     except getopt.GetoptError:
+        print 'GETOPT ERROR'
         sys.exit(2)
     for opt, arg in opts:
         if opt in ('-r', '--reads'):
@@ -23,16 +25,17 @@ def loadargs():
             outputprefix = arg
         if opt in ('-h', '--help'):
             help()
+    print 'loaded arguments succesfully'
     main(readfile, illumina_idx, outputfile, outputprefix)
 
 
 def main(readfile, idx, outfile, outprefix):
     readdict = f.fastqtodict(readfile)
     for i, index in enumerate(idx):
-        with open(outprefix + outfile[i], 'w') as o:
+        with open(outprefix + outfile[i] + '.fastq', 'w') as o:
             for element in readdict:
-                if element.split('_')[1].startswith(idx):
-                    o.write('{}\n{}\n+\n{}\n'.format(element, readdict[element][0], readdict[element][1]))
+                if element.split('_')[1].strip().startswith(index):
+                    o.write('{}\n{}\n+\n{}\n'.format(element.strip(), readdict[element][0], readdict[element][1]))
 
 
 def help():
@@ -40,7 +43,10 @@ def help():
 
 
 if __name__ == '__main__':
+    print 'TEST'
     loadargs()
 
+loadargs()
+print('wtf')
 # script: Take read name, split by _, depending on sequence split onto different
 # outfiles
