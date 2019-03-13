504b3a5c44d33a7f80192bcb0520a65b677819d7
SamWittwer <samwittw@gmail.com>
2019-03-13 09:58:14 +0100

    attachindex now OO and better.

--- fastq/fastq_attachindex.py
+++ fastq/fastq_attachindex.py
@@ -10,35 +10,31 @@ outstream = sys.stdout
 ## reading in all the index sequences
 with open(indexfile, 'r') as idx:
     counter = 0
-    readlist = []
+    readname = None
     idxdict = {}
     for line in idx:
+        if counter == 0:
+            readname = line.strip().split(' ')[0]
+        if counter == 1:
+            idxdict[readname] = line.strip()
+            readname = None
         if counter == 3:
-            readlist.append(line)
-            readobj = samslib.fastq_read(readlist)
-            idxdict[readobj.getreadname()] = readobj
-            readlist = []
             counter = 0
         elif counter < 3:
-            readlist.append(line)
             counter += 1
 ## reading in reads one by one and writing out
 
 counter = 0
 readlist_read = []
 for line in readstream:
-    if line:
-        if counter == 3:
-            readlist_read.append(line)
-            readobj = samslib.fastq_read(readlist_read)
-            if readobj.getreadname() in idxdict:
-                readobj.putindex(idxdict[readobj.getreadname()].sequence())
-                outstream.write(readobj.fastq_writestring())
-            readlist_read = []
-            counter = 0
-        elif counter < 3:
-            readlist_read.append(line)
-            counter += 1
-readstream.close()
-idxdict = None
-sys.exit()
+    if counter == 3:
+        readlist_read.append(line)
+        readobj = samslib.fastq_read(readlist_read)
+        if readobj.getreadname() in idxdict:
+            readobj.putindex(idxdict[readobj.getreadname()])
+            outstream.write(readobj.fastq_writestring())
+        readlist_read = []
+        counter = 0
+    elif counter < 3:
+        readlist_read.append(line)
+        counter += 1
