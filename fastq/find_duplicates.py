a227407fdd1b9c0053ea05c365f55e606f673dcf
SamWittwer <samwittw@gmail.com>
2019-03-13 16:21:31 +0100

    duplicatefinder adapted

--- fastq/find_duplicates.py
+++ fastq/find_duplicates.py
@@ -1,4 +1,4 @@
-import itertools, samslib, binary_tree, ddRAD_functions, sys
+import itertools, samslib, binary_tree, sys
 
 degenerate_product = [''.join(x) for x in itertools.product('ACGT',repeat=4)]
 
@@ -6,7 +6,7 @@ subsets = {}
 
 for item in degenerate_product:
     subsets[item] = []
-infile = open('testset.fastq', 'r')
+infile = sys.stdin
 outfile = sys.stdout
 
 linecounter = 0
@@ -29,15 +29,19 @@ totalreadspassed = 0
 for degenerate in degenerate_product:
     passedcounter = 0
     PCRduplicatecounter = 0
-    searchtree = binary_tree.Node(subsets[degenerate][0].sequence())
-    for singleread in subsets[degenerate][1:]:
-        if searchtree.lookup(singleread.sequence())[0] != None:
-            PCRduplicatecounter += 1
+    if subsets[degenerate]:
+        searchtree = binary_tree.Node(subsets[degenerate][0].sequence())
+        if len(subsets[degenerate]) > 1:
+            for singleread in subsets[degenerate][1:]:
+                if searchtree.lookup(singleread.sequence())[0] != None:
+                    PCRduplicatecounter += 1
+                else:
+                    searchtree.insert(singleread.sequence())
+                    passedcounter += 1
+                    totalreadspassed += 1
+                    outfile.write(singleread.fastq_writestring())
         else:
-            searchtree.insert(singleread.sequence())
-            passedcounter += 1
-            totalreadspassed += 1
+            singleread = subsets[degenerate][0]
             outfile.write(singleread.fastq_writestring())
-    #print '{}: duplicates: {} passed: {}'.format(degenerate, PCRduplicatecounter, passedcounter)
-#print totalreadspassed
+
 outfile.close()
