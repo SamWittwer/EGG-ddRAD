47b9adfad9886b407fd24d2197113c3ec7b1ac9d
SamWittwer <samwittw@gmail.com>
2019-05-21 14:42:32 +0200

    fixed fa

---
+++ vcf_processing/vcf_partitioner.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python
+
+import sys
+
+with open(sys.argv[1], 'r') as v:
+    scaffoldorder = []
+    scaffoldcounts = {}
+    for line in v:
+        if not line.startswith('#'):
+            scaffold = line.strip().split('\t')[0]
+            if scaffold in scaffoldcounts:
+                scaffoldcounts[scaffold] += 1
+            else:
+                scaffoldorder.append(scaffold)
+                scaffoldcounts[scaffold] = 1
+
+print scaffoldorder
