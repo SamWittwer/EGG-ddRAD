3462d677f9d4ee3d666e67d99eeb21c4260a7d2b
SamWittwer <samwittw@gmail.com>
2019-07-24 14:23:11 +0200

    working on Orca sample for phylogeny. Bugs still

--- queueing/generate_bashscripts_from_intervals.py
+++ queueing/generate_bashscripts_from_intervals.py
@@ -1,6 +1,6 @@
 import os
 
-filelist = ['./interval_subsets/' + x for x in os.listdir('./interval_subsets')]
+filelist = ['./interval_subsets/' + x for x in os.listdir('./interval_subsets')].sort()
 
 for f in filelist:
     print f
