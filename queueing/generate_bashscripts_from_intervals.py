153afdab6f48200b933861a6fba543a17f94f04d
SamWittwer <samwittw@gmail.com>
2019-07-24 14:36:54 +0200

    working on Orca sample for phylogeny. Bugs still

--- queueing/generate_bashscripts_from_intervals.py
+++ queueing/generate_bashscripts_from_intervals.py
@@ -5,17 +5,17 @@ filelist = sorted(['./interval_subsets/' + x for x in os.listdir('./interval_sub
 for f in filelist:
     filenum = f.split('_')[-1].split('.')[0]
     outstring = '~/00_software/gatk-4.1.1.0/gatk --java-options "-Xmx4g" GenotypeGVCFs \
-    -R ~/chapter_02_australis/reference/Tur_tru_v1/Turtru_2.fasta \
-    -V gendb://genomicsdb \
-    --intervals {} \
-    --tmp-dir ./tmp \
-    -A MappingQuality \
-    -A MappingQualityRankSumTest \
-    -A DepthPerAlleleBySample \
-    -A AlleleFraction \
-    -verbosity INFO \
-    --include-non-variant-sites \
-    -O vcf/chapter_02_{}.vcf'
+-R ~/chapter_02_australis/reference/Tur_tru_v1/Turtru_2.fasta \
+-V gendb://genomicsdb \
+--intervals {} \
+--tmp-dir ./tmp \
+-A MappingQuality \
+-A MappingQualityRankSumTest \
+-A DepthPerAlleleBySample \
+-A AlleleFraction \
+-verbosity INFO \
+--include-non-variant-sites \
+-O vcf/chapter_02_{}.vcf'
     with open('bash_queueing_scripts/{}.sh'.format(filenum), 'w') as outfile:
         outfile.write('#!/bin/bash\n')
         outfile.write(outstring.format(f, filenum))
