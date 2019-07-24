ee8491f904e32db5fc477af49ad1468b21f9d0bf
SamWittwer <samwittw@gmail.com>
2019-07-24 13:35:17 +0200

    working on Orca sample for phylogeny. Bugs still

--- queueing/EGGtimer.py
+++ queueing/EGGtimer.py
@@ -13,33 +13,33 @@ maxprocesses = sys.argv[2]
 
 print [path_queue + x for x in os.listdir(path_queue)]
 
-#proclist = []
-#runningprocesses = 0
-#queue = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-#while True:
-#    print '##############################'
-#    if proclist:
-#        procstoremove = []
-#        for i, p in enumerate(proclist):
-#            if p.poll() == 0:
-#                print 'deleting proc {}'.format(p.pid)
-#                procstoremove.append(i)
-#        for number in procstoremove:
-#            del proclist[number]
-#
-#    if len(proclist) <= 5:
-#        print 'starting new subprocess'
-#        try:
-#            queue.pop()
-#        except IndexError:
-#            print 'queue empty!'
-#            if not proclist:
-#                print 'proclist empty!'
-#                break
-#        else:
-#            proclist.append(sub.Popen(['sleep', '2']))
-#            print [x.pid for x in proclist]
-#            print [x.poll() for x in proclist]
-#            print 'waiting one second before starting next'
-#    time.sleep(1)
-#print [x.poll() for x in proclist]
+proclist = []
+runningprocesses = 0
+queue = [path_queue + x for x in os.listdir(path_queue)]
+while True:
+    print '##############################'
+    if proclist:
+        procstoremove = []
+        for i, p in enumerate(proclist):
+            if p.poll() == 0:
+                print 'deleting proc {}'.format(p.pid)
+                procstoremove.append(i)
+        for number in procstoremove:
+            del proclist[number]
+
+    if len(proclist) <= 5:
+        print 'starting new subprocess'
+        try:
+            currenttask = queue.pop()
+        except IndexError:
+            print 'queue empty!'
+            if not proclist:
+                print 'proclist empty!'
+                break
+        else:
+            proclist.append(sub.Popen([currenttask]))
+            print [x.pid for x in proclist]
+            print [x.poll() for x in proclist]
+            print 'waiting one second before starting next'
+    time.sleep(1)
+print [x.poll() for x in proclist]
