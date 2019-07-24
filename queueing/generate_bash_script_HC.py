bb1a08ef2cc26fc2254b493db48150504fea6f2b
SamWittwer <samwittw@gmail.com>
2019-07-24 15:06:56 +0200

    Preparing queue scripts

--- queueing/generate_bash_script_HC.py
+++ queueing/generate_bash_script_HC.py
@@ -3,4 +3,11 @@ import os
 filelist = sorted(['./bam/' + x for x in os.listdir('./bam') if x.endswith('.bam')])
 
 for f in filelist:
-    print f
+    samplename = f.split('/')[-1].split('.')[0]
+    outstring = '~/00_software/gatk/gatk-4.1.2.0/gatk --java-options "-Xmx4g" HaplotypeCaller \
+-R ~/chapter_02_australis/reference/Tur_tru_v1/Turtru_2.fasta \
+-I {} -o ./per_sample_vcf/{}.vcf.gz -ERC GVCF -A MappingQuality -A MappingQualityRankSumTest -A DepthPerAllelePerSample \
+-A AlleleFraction -A QualByDepth'.format(f, samplename)
+    with open('./bash_queueing_scripts_variantcalling/{}.sh'.format(samplename)) as outfile:
+        outfile.write('#!/bin/bash\n')
+        outfile.write(outstring)
