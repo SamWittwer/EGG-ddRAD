52e8219efcc6902030b687ea14e55d3236d14e12
SamWittwer <samwittw@gmail.com>
2019-07-24 15:07:46 +0200

    Preparing queue scripts

--- queueing/generate_bash_script_HC.py
+++ queueing/generate_bash_script_HC.py
@@ -8,6 +8,6 @@ for f in filelist:
 -R ~/chapter_02_australis/reference/Tur_tru_v1/Turtru_2.fasta \
 -I {} -o ./per_sample_vcf/{}.vcf.gz -ERC GVCF -A MappingQuality -A MappingQualityRankSumTest -A DepthPerAllelePerSample \
 -A AlleleFraction -A QualByDepth'.format(f, samplename)
-    with open('./bash_queueing_scripts_variantcalling/{}.sh'.format(samplename)) as outfile:
+    with open('./bash_queueing_scripts_variantcalling/{}.sh'.format(samplename), 'w') as outfile:
         outfile.write('#!/bin/bash\n')
         outfile.write(outstring)
