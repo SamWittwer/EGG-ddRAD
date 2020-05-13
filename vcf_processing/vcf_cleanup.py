68bff4f0fdfb42b9052e5450f7b729936f54a8a6
samwittw@gmail.com <T_assadaR1984>
2020-05-13 09:58:49 +0200

    cleanup vcf started

--- vcf_processing/vcf_cleanup.py
+++ vcf_processing/vcf_cleanup.py
@@ -3,11 +3,27 @@ import sys
 i = sys.stdin
 o = sys.stdout
 
+def processind(indstring, gt, ad, dp, gq, pl):
+    if indstring.startswith('./.'):
+        return './.'
+    else:
+        instringsplit = indstring.split(':')
+        indstringsplit[gt] = indstringsplit[gt].replace('|', '/')
+        newstringlist = [indstringsplit[gt], indstringsplit[ad], indstringsplit[dp], indstringsplit[gq], indstringsplit[pl]]
+        return ':'.join(newstringlist)
+
 for line in i:
     if line.startswith('#'):
         o.write(line)
     else:
         linespl = line.strip().split('\t')
-        print(linespl)
-        break
+        formatcol = linespl[8].split(':')
+        GT = formatcol.index('GT')
+        AD = formatcol.index('AD')
+        DP = formatcol.index('DP')
+        GQ = formatcol.index('PL')
+        PL = formatcol.index('PL')
+        cleanline = '\t'.join(linespl[:8])
+        print(cleanline)
+
 
