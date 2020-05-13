27de5b9ea3adda316c0647f2d8fcead3f8141273
samwittw@gmail.com <T_assadaR1984>
2020-05-13 11:03:43 +0200

    cleanup vcf started

--- vcf_processing/vcf_cleanup.py
+++ vcf_processing/vcf_cleanup.py
@@ -18,12 +18,15 @@ for line in i:
     else:
         linespl = line.strip().split('\t')
         formatcol = linespl[8].split(':')
-        GT = formatcol.index('GT')
-        AD = formatcol.index('AD')
-        DP = formatcol.index('DP')
-        GQ = formatcol.index('PL')
-        PL = formatcol.index('PL')
-        cleanline = '\t'.join(linespl[:8]) + '\tGT:AD:DP:GQ:PL\t' + '\t'.join([processind(x, GT, AD, DP, GQ, PL) for x in linespl[9:]]) + '\n'
+        if linespl[5] == '.':
+            cleanline = '\t'.join(linespl[:9]) + '\t' + '\t'.join(['./.' if x.startswith('./.') else x for x in linespl[9:]]) + '\n'
+        else:
+            GT = formatcol.index('GT')
+            AD = formatcol.index('AD')
+            DP = formatcol.index('DP')
+            GQ = formatcol.index('PL')
+            PL = formatcol.index('PL')
+            cleanline = '\t'.join(linespl[:8]) + '\tGT:AD:DP:GQ:PL\t' + '\t'.join([processind(x, GT, AD, DP, GQ, PL) for x in linespl[9:]]) + '\n'
         o.write(cleanline)
 
 
