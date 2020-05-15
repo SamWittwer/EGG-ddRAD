b29824260583815fa4ce419aa1f8a7b57d0d4720
samwittw@gmail.com <T_assadaR1984>
2020-05-15 09:42:11 +0200

    conversion step 1 done, now missing block analysis

--- ABLE/analyze_missingdata.py
+++ ABLE/analyze_missingdata.py
@@ -12,10 +12,7 @@ for line in instream:
     if line.startswith('<indnames>'):
         indtag = True
         indmissingcountdict = {}
-        continue
-    elif line.startswith('<block>'):
-        blocktag = True
-        blockseqs = []
+        ordered_indnames = []
         continue
     if indtag:
         if line.startswith('</indnames>'):
@@ -23,4 +20,22 @@ for line in instream:
             continue
         else:
             indmissingcountdict[line.strip()] = 0
+            ordered_indnames.append(line.strip())
+    if line.startswith('<block>'):
+        blocktag = True
+        individual_idx = 0
+        continue
+    if blocktag:
+        if line.startswith('BLOCK'):
+            blockname = line.strip()
+            blocklength = int(blockname.split('len')[1])
+            missingid = id('N'*blocklength)
+        elif line.startswith('</block>'):
+            blocktag = False
+            continue
+        else:
+            if id(line.strip()) == missingid:
+                indmissingcountdict[ordered_indnames[individual_idx]] += 1
+                individual_idx += 1
+
 print(indmissingcountdict)
