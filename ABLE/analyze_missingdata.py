38ef27ed1fe7eb909696c1dc03dcdb4560250414
samwittw@gmail.com <T_assadaR1984>
2020-05-15 09:27:12 +0200

    conversion step 1 done, now missing block analysis

--- ABLE/analyze_missingdata.py
+++ ABLE/analyze_missingdata.py
@@ -1,4 +1,24 @@
 # this script takes the output from convert_vcf_pseudoMS.py
 # goes through data block by block
 # output: table of missing blocks per individual
+import sys
 
+instream = sys.stdin
+outstream = sys.stdout
+
+indtag = False
+blocktag = False
+for line in instream:
+    if '<indnames>' in line:
+        indtag = True
+        indmissingcountdict = {}
+    elif '<block>' in line:
+        blocktag = True
+        blockseqs = []
+    if indtag:
+        if not line.startswith('<indlist>'):
+            indmissingcountdict[line.strip()] = 0
+        elif '</indnames>' in line:
+            indtag = False
+            pass
+print(indmissingcountdict)
