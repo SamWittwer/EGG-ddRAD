a3d0fb0a8f34289e64f0c64dd3407fa30d0f86dc
Sam <samwittw@gmail.com>
2020-05-24 15:22:00 +0200

    Rewrote convert_vcf_pseudoMS.py for python 3 and optimized (conversion on the fly)

--- ABLE/reorder_tempfile.py
+++ ABLE/reorder_tempfile.py
@@ -3,9 +3,31 @@ import sys
 
 #orderfile = sys.argv[1]
 prelimpseudoms = sys.stdin
-orderfile = 'individualorder.txt'
+orderfile = 'individualorder_sortedbysite.txt'
+output = sys.stdout
 
 with open(orderfile, 'r') as desiredorder:
     indlist = [x.strip() for x in desiredorder]
 
-print(indlist)
+blockdict = {}
+blockorder = []
+for line in prelimpseudoms:
+    if line.startswith('//'):
+        continue
+    elif line.starswith('BLOCK'):
+        blockorder.append(line.strip())
+        blockdict[blockorder[-1]] = {}
+    else:
+        linespl = line.strip().split()
+        if linespl[0] in blockdict.keys():
+            blockdict[linespl[0]].append(linespl[1])
+        else:
+            blockdict[linespl[0]] = [linespl[1]]
+
+for BLOCK in blockorder:
+    output.write('//\n{}\n'.format(BLOCK))
+    for individual in indlist:
+        output.write('{} {}\n{} {}\n'.format(individual,
+                                           blockdict[BLOCK][individual][0],
+                                           individual,
+                                           blockdict[BLOCK][individual][1]))
