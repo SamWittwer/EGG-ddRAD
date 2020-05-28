f5ec453a73c39c599be801c8bdddfa08bf69814c
Sam <samwittw@gmail.com>
2020-05-28 08:27:50 +0200

    testing condbsfs

--- ABLE/cond_bSFS.py
+++ ABLE/cond_bSFS.py
@@ -11,22 +11,22 @@ def main():
     try:
         opts, args = getopt.getopt(sys.argv[1:], 'c:a:o:fh')
     except getopt.GetoptError as err:
-        print str(err)
+        print(str(err))
         sys.exit(1)
 
     for o, a in opts:
-        if o in ('-a'):
+        if o in '-a':
             try:
                 ablefile = open(a, 'r')
                 for line in ablefile:
                     if line.startswith('Found a maximum'):
                         tbivalues = [float(x) for x in line.strip().split(' ')[4:-3]]
             except IOError:
-                print 'file doesn\'t exist'
+                print('file doesn\'t exist')
                 sys.exit(1)
             finally:
                 ablefile.close()
-        elif o in ('-c'):
+        elif o in '-c':
             try:
                 configfile = open(a, 'r')
                 for line in configfile:
@@ -37,31 +37,30 @@ def main():
                     elif line.startswith('datafile'):
                         datafileline = line
             except IOError:
-                print 'file doesn\'t exist'
+                print('file doesn\'t exist')
                 sys.exit(1)
             finally:
                 configfile.close()
-        elif o in ('-o'):
+        elif o in '-o':
             outfilename = a
-        elif o in ('-f'):
+        elif o in '-f':
             folded = True
-        elif o in ('-h'):
+        elif o in '-h':
             displayhelp()
             sys.exit()
             
     tbidict={}
     for i, tbielement in enumerate(tbivalues):
         tbidict['tbi' + str(i+1)] = tbielement
-    print tbidict
-    
-    print tbivalues
-    
+    print(tbidict)
 
-    print popsline
-    print ABLEcommand
+    print(tbivalues)
+
+    print(popsline)
+    print(ABLEcommand)
     ABLElist = ABLEcommand.strip().split(' ')
     print [tbidict[x] if x in tbidict else x for x in ABLElist]
-    print datafileline
+    print(datafileline)
     outstring = ''
     outstring += popsline + 'task conditional_bSFS\n' + datafileline + ' '.join([str(tbidict[x]) if x in tbidict else str(x) for x in ABLElist])
     if folded:
@@ -73,7 +72,7 @@ def main():
         outstream.close()
 
 def displayhelp():
-    print """usage: 
+    print("""usage: 
     cond_bSFS.py -a ABLEinfer_output -c ABLEinfer_config -o outfilename [options]
     
     
@@ -81,6 +80,7 @@ def displayhelp():
     -c      The config file used to infer
     -o      The desired output filename
     -f      Add folded keyword to output
-    -h      this helpful text"""
+    -h      this helpful text""")
+
 
 main()
